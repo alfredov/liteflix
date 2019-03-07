28356670c2b7cb9ac2e092ef2979dd2b68721591
Alfredo V <alfredo.villagomez.m@gmail.com>
2019-03-07 14:47:42 -0400

    Fix lint errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,7 @@ export default {
     title: 'LiteFlix',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid:'description', name:'description', content:'The latest Tv series' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ]
   },
   serverMiddleware: ['~/api/index.js']
